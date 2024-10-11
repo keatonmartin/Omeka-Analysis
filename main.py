786cf1284f3ee1f67d7fa0c260e6e118a89dd00d
keatonmartin <keatonsphone1@gmail.com>
2024-10-10 22:48:25 -0400

    add main.py and utils.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import requests
+import json
+import utils
+from nltk.probability import FreqDist
+
+URL = "https://drvk.createuky.net/news-articles/api/items"
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+def main():
+    r = requests.get(URL)
+    items = json.loads(r.text)
+
+    descriptions = []
+    for item in items:
+        for text in item["element_texts"]:
+            if text["element"]["name"] == "Description":
+                descriptions.append(text["text"])
+    
+    combined = " ".join(descriptions)
+    tokens = utils.tokens(combined)
+
+    freq = FreqDist(tokens)
+    for word, f in freq.most_common():
+        print(word, f)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,41 @@
+import re
+import html
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('punkt_tab')
+nltk.download('punkt')
+
+
+# clean_html_tags removes all sequences from a text of the form <...> 
+def clean_html_tags(text: str) -> str:
+    pattern = re.compile(r'<.*?>')
+    return pattern.sub('', text)
+
+# clean_html_entities replaces html entities like &nbsp; with their proper characters
+def clean_html_entities(text: str) -> str:
+    return html.unescape(text)
+
+# tokens takes a text, cleans it for html tags and entities, 
+# and returns a list of all words within the text
+# not including punctuation or common english stopwords
+# 
+# a list of custom stopwords may be passed to stopwords
+def tokens(text: str, stops: list = None):
+    if stops == None:
+        stops = []
+    # clean text
+    cleaned = clean_html_entities(clean_html_tags(text))
+    
+    # tokenize
+    words = word_tokenize(cleaned)
+
+    stopwords_list = stopwords.words("english")
+    stopwords_list.extend(stops)
+
+    # remove punctuation, make lower case, don't include stopwords
+    return [word.lower() for word in words if (word.isalpha() and word not in stopwords_list)]
+
+    
+
+        
